fba035d5a8a3967990cc125508736075e40446de
Niyrux <samuel47000@hotmail.com>
2023-04-18 14:49:32 +0200

    component banniere

---
+++ src/component/bandeau/bandeau.js
@@ -0,0 +1,11 @@
+import './bandeau.css'
+
+function Bandeau() {
+    return (
+        <div className="bandeau">
+            <p>Chez vous, partout et ailleurs</p>
+        </div>
+    );
+}
+
+export default Bandeau;
